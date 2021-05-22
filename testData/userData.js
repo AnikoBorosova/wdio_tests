2cd9127c4fece1d8d13e0a8ab6a83da383bccd69
Anikó Borosová <borosanikoo@gmail.com>
2021-05-22 14:48:44 +0200

    add userData

---
+++ testData/userData.js
@@ -0,0 +1,13 @@
+module.exports = {
+    loginData: {
+        password: "secret_sauce",
+        standardUser: "standard_user",
+        lockedOutUser: "locked_out_user",
+        problemUser: "problem_user",
+        performanceGlitchUser: "performance_glitch_user"
+    },
+    purchaseData: {
+        itemName: "Sauce Labs Backpack",
+        price: 29.99
+    }
+};
