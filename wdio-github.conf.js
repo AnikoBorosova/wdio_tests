a130f8c10c8a23601b50f4f3f72522aa2eb1fb44
Anikó Borosová <borosanikoo@gmail.com>
2022-01-08 19:17:36 +0100

    add wdio confs

---
+++ wdio-github.conf.js
@@ -0,0 +1,21 @@
+const sharedConfig = require("./wdio-shared.conf");
+
+exports.config = {
+	...sharedConfig,
+	...{
+		capabilities: [
+			{
+				maxInstances: 3,
+				browserName: 'chrome',
+				'goog:chromeOptions': {
+					args: ['--start-maximized', '--headless', '--disable-gpu', '--window-size=1920,1080', '--no-sandbox', '--no-cache']
+				}
+			},
+		],
+		reporters: ['spec', ['allure', {
+			outputDir: 'allure-results',
+			disableWebdriverStepsReporting: true,
+			disableWebdriverScreenshotsReporting: false,
+		}]]
+	}
+}
---
+++ wdio-local.conf.js
@@ -0,0 +1,17 @@
+const sharedConfig = require("./wdio-shared.conf");
+
+exports.config = {
+	...sharedConfig,
+	...{
+		capabilities: [
+			{
+				maxInstances: 3,
+				browserName: 'chrome',
+				'goog:chromeOptions': {
+					args: ['--start-maximized', '--headless', '--disable-gpu', '--window-size=1920,1080', '--no-sandbox', '--no-cache']
+				}
+			}
+		],
+		reporters: ['spec']
+	}
+}
